f258426204191b6df7ca3814f044a770a946c768
Bartek <zielsone@gmail.com>
2023-08-25 19:32:05 +0200

    Adding a photo gallery

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
-console.log(scrollTop());
+const gallery = document.querySelector('.showGall');
+const gallBtn = document.querySelector('.photoBtn');
+
+const handleGall = () => {
+    gallery.classList.toggle('showGall--active')
+
+}
+
+gallBtn.addEventListener('click', handleGall)
 
 
 
