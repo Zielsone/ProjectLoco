4511b09dcfdd70c0983674316c84a91ac2e7dc97
Bartek <zielsone@gmail.com>
2023-08-29 19:32:16 +0200

    Addition of a gallery scrolling function and animation

--- js/main.js
+++ js/main.js
@@ -1,27 +1,47 @@
 const gallery = document.querySelector ('.showGall');
 const gallBtn = document.querySelector('.photoBtn');
 const closeGall = document.querySelector('.close');
-const arrRght = document.querySelector('.right');
-const slide = document.querySelector('.slide');
+const nextPic = document.querySelector('.right')
+let slides = document.getElementsByClassName('mySlides')
 
-const pic =[ "IMG_6395.JPG", "IMG_6411.JPG", "IMG_6413.JPG", "IMG_6421.JPG"];
+const gallSection = document.querySelector('.gallery-mobile');
+
+
+let slideIndex = 1; 
 
 const handleGall = () => {
     gallery.classList.toggle('showGall--active')
     closeGall.classList.toggle('showGall--active')
+    
 }
 
-const picBrowser = () => {
-   console.log(slide)
-   slide.outerHTML = `<img src="pic/${pic[0]}" style="width:100%">`
-   
+const plusSlides = (n) => {
+    showSlides(slideIndex += n)
 }
+
+const showSlides = (n) => {
+    let i;
+
+    if(n > slides.length) {slideIndex =1}
+    if(n < 1) {slideIndex = slides.length}
+
+    for (i=0; i< slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex-1].style.display = "block";
+
+}
+
+
 
 
 
 gallBtn.addEventListener('click', handleGall)
 closeGall.addEventListener('click', handleGall)
-arrRght.addEventListener('click', picBrowser)
+nextPic.addEventListener('click', showSlides)
+
+
 
 
 
